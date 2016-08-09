3fee432ef67c74ce0b4bd06b36adf53f013793b9
Estudiante <Estudiante@FI454402-24.unal.loc>
2016-08-09 15:17:06 -0500

    Commit inicial

--- src/Match.java
+++ src/Match.java
@@ -13,7 +13,22 @@ public class Match {
         int x = 0;
         int y = 0;
         while (x < 5){
-            System.out.print(x + " " + y + " ");
+        //  y = x - y;
+        //  y  = y + x;
+        /*  y = y + 2;
+            if( y > 4){
+                y = y - 1;
+            }*/
+        /*  x = x + 1;
+            y = y + x; */
+            if ( y < 5){
+                x = x + 1;
+                if (y < 3){
+                    x = x - 1;
+                }
+            }
+            y = y + 2;
+            System.out.print(x + "" + y + " ");
             x = x + 1;
         }
     }
