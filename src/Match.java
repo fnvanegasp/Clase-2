a8b5e6c2ae8982e4390d38c0576b2f2a819a6d33
Estudiante <Estudiante@FI454402-24.unal.loc>
2016-08-09 14:54:43 -0500

    Commit inicial

---
+++ src/Match.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Estudiante
+ */
+public class Match {
+    public static void main(String [] args){
+        int x = 0;
+        int y = 0;
+        while (x < 5){
+            System.out.print(x + " " + y + " ");
+            x = x + 1;
+        }
+    }
+}
